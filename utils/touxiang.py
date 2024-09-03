e56d0dba0037b6c1242ff12bff04487aa9f08c53
mccoylin <mccoy.lin@gmail.com>
2024-09-03 09:08:42 +0800

    修改為 mysql.connector 及 RE 處理。

--- utils/touxiang.py
+++ utils/touxiang.py
@@ -1,31 +1,57 @@
-import pymysql
-import numpy as np
-conn = pymysql.connect(host="127.0.0.1", user="root", password="123456", database="stocktrading")
-cursor = conn.cursor()
+#!/usr/bin/python3 
+# -*- coding: utf-8 -*-
 
-sql  = "select * from user_table"
 
-cursor.execute(sql)
+import mysql.connector
+import pandas as pd
+import re
 
-des = cursor.fetchall()
 
-des  =  np.array(des)
+# 移除 mysql 裡的 stocktrading.user_table 欄位 'photo_url' 前的 '..' 兩個字元
+# 更新 stocktrading.user_table 欄位 'phone_number'
+#
+# 使用 mysql.connector.connect() 連線到 mysql
+# 使用 RE 處理字串
+# 使用 pandas 轉成 DataFrame, 容易使用欄位名稱
 
-# res = des[:,[7]]
-print(des)
-# print(res)
-for i in range(0,len(des)):
-    des[i][7] = des[i][7][2:len(des[i][7])]
-    print(des[i][7])
 
-print(des)
-des = des.tolist()
-print(des)
+# 查詢出 user_table 裡的資料, 取出 phone_number, photo_url 欄位就夠。
+sql_user_table = "select phone_number, photo_url from user_table;"
 
-sql2 = "update user_table set photo_url=%s  where phone_number=%s;"
+# 更新 user_table 裡的 photo_url，phone_number 是 key
+sql_update = "update user_table set photo_url=%s where phone_number=%s;"
 
-for i in range(0,len(des)):
-    cursor.execute(sql2,[des[i][7],des[i][5]])
-    conn.commit()
-cursor.close()
-conn.close()
+
+conn = mysql.connector.connect(
+    host = "127.0.0.1",
+    port = 3306,
+    user = "trading",
+    password = "YOUR_PASSWORD_記得修改",
+    db = "stocktrading",
+    charset = "utf8mb4"
+)
+
+
+with conn:
+    with conn.cursor(dictionary=True) as cursor:
+        cursor.execute(sql_user_table)
+        data = cursor.fetchall()
+
+        # 用 pandas 轉成 DataFrame
+        data = pd.DataFrame(data)
+
+        values_list = []
+        # 取出 photo_url 欄位的資料
+        for index, row in data.iterrows():
+            # 用 RE 移除 photo_url 前的 '..'
+            photo_url = re.sub(r'^\.\.', '', row['photo_url'])
+            value = (photo_url, row['phone_number'])
+            # 加回 list
+            values_list.append(value)
+
+        # 更新 mysql
+        cursor.executemany(sql_update, values_list)         # 一次傳 list, 比較省時間
+        conn.commit()
+
+        print(f'處理 tranding.user_table "photo_url" 欄位，資料 {len(data)} 筆')
+        # print(values_list)
